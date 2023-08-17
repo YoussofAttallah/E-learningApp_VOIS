bd6921b89b03b9c6e49a6ef43ffdbd7109305de3
Muhammad Moataz <mohamedmoatazali60@gmail.com>
2023-08-17 16:30:06 +0300

    created InstructorNotFoundException

--- src/main/java/com/example/demo/service/InstructorService.java
+++ src/main/java/com/example/demo/service/InstructorService.java
@@ -1,8 +1,10 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Instructor;
+import com.example.demo.exceptions.InstructorNotFoundException;
 import com.example.demo.repository.InstructorDAO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,11 +12,22 @@ import java.util.List;
 public class InstructorService {
     private final InstructorDAO instructorDAO;
 
+
     public InstructorService(InstructorDAO instructorDAO) {
         this.instructorDAO = instructorDAO;
     }
 
+    @Transactional
     public List<Instructor> getAllInstructors() {
         return instructorDAO.findAll();
+    }
+    @Transactional
+    public void createNewInstructor(Instructor instructor) {
+        instructorDAO.save(instructor);
+    }
+
+    public Instructor getInstructor(Integer instructorId) {
+        return instructorDAO.findById(instructorId)
+                        .orElseThrow(() -> new InstructorNotFoundException("instructor not found"));
     }
 }
