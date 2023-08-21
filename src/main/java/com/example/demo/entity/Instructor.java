bc3d11d3d4c815c49db4a178e09d0fe2efde27a3
Muhammad Moataz <mohamedmoatazali60@gmail.com>
2023-08-21 10:25:14 +0300

    Solved User issue with Postgresql it turns out the word 'user' is reserved for Postgresql itself

--- src/main/java/com/example/demo/entity/Instructor.java
+++ src/main/java/com/example/demo/entity/Instructor.java
@@ -6,7 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.hibernate.annotations.NotFound;
 
 import java.util.List;
 
