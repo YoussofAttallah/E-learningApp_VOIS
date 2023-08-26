d5cf00631ee914f2b5645a4b2342cde05d7ad41f
Muhammad Moataz <mohamedmoatazali60@gmail.com>
2023-08-26 08:21:52 +0300

    Adding NotFoundExceptionHandler to custom handle NotFoundException

---
+++ src/main/java/com/example/demo/exception_handlers/NotFoundExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.exception_handlers;
+
+import com.example.demo.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class NotFoundExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
